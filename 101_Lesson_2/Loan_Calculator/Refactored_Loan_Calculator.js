3bfc7926bdca3cb0306290207571c802fdb2ab46
Robert Kendrick <rck.1996@gmail.com>
2022-10-20 09:08:17 -0700

    Now with refactored calculator

---
+++ 101_Lesson_2/Loan_Calculator/Refactored_Loan_Calculator.js
@@ -0,0 +1,77 @@
+const readline = require('readline-sync');
+
+function prompt (msg) {
+  console.log(`=> ${msg}`);
+}
+function greetUser() {
+  return prompt(`Welcome to your loan calculator \n \
+  In order to calculate your monthly payment we'll need to know the \n \
+  principal, interest rate and loan term.`);
+}
+greetUser();
+
+function displayError() {
+  return 'Error: input not recognized as a number. \n Please try again.';
+}
+
+function validateNumber(input){
+  if (isNaN(input) === true || input.trimStart() === '' || input < 0 ){
+    return false;
+  }
+  else {
+    return input;
+  }
+}
+
+function getPrincipal(){
+  prompt('How much is the principal of the loan?');
+  let internal = validateNumber(readline.question());
+  while (internal === false){
+    prompt(displayError());
+    internal = validateNumber(readline.question());
+  }
+  return internal;
+}
+
+let principal = getPrincipal();
+
+function getDuration(){
+  prompt ('How many monthly installments are to be paid?');
+  let internal = validateNumber(readline.question());
+  while (internal === false){
+    prompt(displayError());
+    internal = validateNumber(readline.question());
+  }
+  return internal;
+}
+
+let duration = getDuration();
+
+function getRate() {
+  prompt(`What is the interest rate? \n \
+  please use an integer,unless the rate is <1, and do not add a '%' to the end`);
+  let internal = validateNumber(readline.question());
+  while (internal === false){
+    prompt(displayError());
+    internal = validateNumber(readline.question());
+  }
+  return internal;
+}
+
+let rate = getRate();
+
+let monthlyRate = rate/12;
+let decimalRate = monthlyRate/100;
+
+function calculatePayment(principal, decimalRate, duration){
+  if (rate === 0){
+    payment = principal / duration
+  }
+  else  {
+    payment = principal * ((decimalRate) / (1 - Math.pow((1 + (decimalRate)), (-duration))));
+  }
+  return prompt(`your monthly payment will be $${payment.toFixed(2)}, \n \
+  the total interest on the loan will be $${((payment * duration) - principal).toFixed(2)} `);
+}
+
+calculatePayment(principal, decimalRate, duration);
